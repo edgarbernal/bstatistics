44a177c86d3dda218e55d1a5c8cfa93326dd4550
Edgar Bernal <edgar@localhost.localdomain>
2015-09-05 18:24:56 -0400

    add src

---
+++ src/main/java/com/matware/service/impl/LookupManagerImpl.java
@@ -0,0 +1,37 @@
+package com.matware.service.impl;
+
+import com.matware.dao.LookupDao;
+import com.matware.model.LabelValue;
+import com.matware.model.Role;
+import com.matware.service.LookupManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Implementation of LookupManager interface to talk to the persistence layer.
+ *
+ * @author <a href="mailto:matt@raibledesigns.com">Matt Raible</a>
+ */
+@Service("lookupManager")
+public class LookupManagerImpl implements LookupManager {
+    @Autowired
+    LookupDao dao;
+
+    /**
+     * {@inheritDoc}
+     */
+    public List<LabelValue> getAllRoles() {
+        List<Role> roles = dao.getRoles();
+        List<LabelValue> list = new ArrayList<LabelValue>();
+
+        for (Role role1 : roles) {
+            list.add(new LabelValue(role1.getName(), role1.getName()));
+        }
+
+        return list;
+    }
+}
---
+++ src/main/java/com/matware/service/impl/PasswordTokenManager.java
@@ -0,0 +1,19 @@
+package com.matware.service.impl;
+
+import com.matware.model.User;
+
+
+public interface PasswordTokenManager {
+
+    /**
+     * {@inheritDoc}
+     */
+    String generateRecoveryToken(User user);
+
+    /**
+     * {@inheritDoc}
+     */
+    boolean isRecoveryTokenValid(User user, String token);
+
+    void invalidateRecoveryToken(User user, String token);
+}
